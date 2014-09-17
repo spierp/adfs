40fd7cf5344ce75a99a7f95cc0b788c708f80fa4
Peter Spier <spierp@gmail.com>
2014-09-17 15:43:48 -0700

    assets now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,10 +27,12 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
+  
+  config.assets.version = '1.0'
   config.assets.precompile += %w( '.woff', '.eot', '.svg', '.ttf' )
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
